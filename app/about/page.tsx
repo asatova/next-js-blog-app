12ec4f1ee58e41d465c664a019a7ca5233faaf32
zarnigor <zarnigorasatova52@gmail.com>
2024-07-19 11:33:02 +0500

    feature

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div>
             <h1>About Us</h1>
-            <p>This is the about page of our Next.js application.</p>
+
         </div>
     );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import About from "../app/about";
+
 
 
 
@@ -8,35 +8,22 @@ export const Navbar = () => {
     return (
         <nav className='container mx-auto flex justify-center items-center bg-gray-100 text-zinc-900 p-4'>
             <div className='container mx-auto flex justify-start items-end'>
-                <Image
+              <a href="/"> <Image
                     src="/logo.png"
                     alt="Picture of the author"
                     width={100}
                     height={100}
                     quality={100}
                 />
+              </a>
             </div>
         <div className="hidden md:flex text-white">
            <Link href="/" className="mx-2 text-black"> Home</Link>
-
         </div>
-        <div>
-            <Link
-                href={{
-                    pathname: '/about'
+            <div><Link href={{  pathname: '/about'}} > About us</Link></div>
+            <div><Link href={{  pathname: '/contact'}} > Contact</Link></div>
+            <div><Link href={{  pathname: '/news'}} > News</Link></div>
 
-                }}
-            >
-                About us
-            </Link>
-        </div>
-        <div>
-            <Link href="/contact" className="mx-2 text-black">Contact</Link>
-         </div>
-                <div>
-                    <Link href="/news" className="mx-2 text-black">News</Link>
-
-            </div>
         </nav>
     )
 }
